2c6c325c547e081b1904fc2f1f45f3ace0799357
rheinata02 <117893155+rheinata02@users.noreply.github.com>
2022-11-17 16:20:17 +0700

    Add files via upload

---
+++ 1_C_71220879.py
@@ -0,0 +1,12 @@
+namadepan = 'Cassandra'
+namabelakang ='Charlotte'
+matakuliah = "Logika Matematika"
+grup = "A"
+input("Masukkan nama Anda:")
+input("Masukkan mata kuliah:")
+input("Masukkan grup:")
+print('Haloo!',namabelakang,namadepan)
+print(namabelakang,"tergabung dalam kelas",matakuliah,"pada grup",grup)
+
+
+                               
---
+++ 2_C_71220879.py
@@ -0,0 +1,7 @@
+kecepatan = int(input("Masukkan Kecepatan:"))
+waktu = int(input("Masukkan waktu yang diperlukan:"))
+bensin = kecepatan*waktu//10
+biaya = bensin *15000
+print("Teman Anda mengisi bensin sebanyak",bensin,"liter")
+print("Biaya yang dikeluarkan untuk mengisi bensin adalah Rp.",biaya)
+
